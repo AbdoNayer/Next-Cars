c388c953bf2240362cc8c0f6cd38417174bb4cd7
AbdoNayer <abdelwahabnayer20@gmail.com>
2023-02-02 17:18:51 +0200

    done

--- components/Filtration/index.js
+++ components/Filtration/index.js
@@ -1,12 +1,41 @@
 import { useTranslation } from 'react-i18next';
+import API from '../../api.json';
 
 export default  function Filtration () {
     
     const { t }              = useTranslation();
 
     return (
-        <div className='item-car'> 
-            
+        <div className='filtration p-3 my-5 rounded-2'> 
+            <div className='d-flex align-items-center justify-content-between'>
+
+                <div className='d-flex align-items-center'>
+                    <div className='select-item'>
+                        <select defaultValue={'DEFAULT'} onChange={ e => console.log('e', e.target.value)}>
+                            <option value="DEFAULT" disabled>{t('carBrand')}</option>
+                            {
+                                API.brands.map((item) => (
+                                    <option value={item.name} key={item.id}>{t(item.name)}</option>
+                                ))
+                            }
+                        </select>
+                    </div>
+                    
+                    <div className='select-item mx-3'>
+                        <select defaultValue={'DEFAULT'} onChange={ e => console.log('e', e.target.value)}>
+                            <option value="DEFAULT" disabled>{t('vehicleType')}</option>
+                            {
+                                API.brands.map((item) => (
+                                    <option value={item.name} key={item.id}>{t(item.name)}</option>
+                                ))
+                            }
+                        </select>
+                    </div>
+                </div>
+
+                <button className='btn-button bg-white'>{t('search')}</button>
+
+            </div>
         </div>
     )
 
--- components/Footer/index.js
+++ components/Footer/index.js
@@ -7,8 +7,24 @@ export default function Footer() {
   const { t }                         = useTranslation();
 
   return (
-    <footer>
-        <h1>Footer</h1>
+    <footer className='bg-second pt-4 mt-5 pb-5'>
+        <div className='container text-center'>
+            <h4 className='mb-4 fw-bold'>{t('title')}</h4>
+            <div className='d-flex align-items-center justify-content-center social'>
+                <Link href={'/'} className='facebook d-flex align-items-center justify-content-center text-white mx-2'>
+                  <span className='icon-facebook' />
+                </Link>
+                <Link href={'/'} className='instagram d-flex align-items-center justify-content-center text-white mx-2'>
+                  <span className='icon-instagram' />
+                </Link>
+                <Link href={'/'} className='twitter d-flex align-items-center justify-content-center text-white mx-2'>
+                  <span className='icon-twitter' />
+                </Link>
+                <Link href={'/'} className='youtube d-flex align-items-center justify-content-center text-white mx-2'>
+                  <span className='icon-youtube' />
+                </Link>
+            </div>
+        </div>
     </footer>
   )
 }
